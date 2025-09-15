c9103c58b08a19f93207c7057b9c80d3632eeb39
bielzinmene <gabrielmenezes.tais@gmail.com>
2025-09-15 20:26:13 -0300

    Update dic2.py

--- dic2.py
+++ dic2.py
@@ -5,16 +5,15 @@ count = 0
 for _ in range(n):
     nome, nota = input().split(',')  
     nota = float(nota)  
-    pess_nt[nome] = nota  # adiciona ao dicionário
+    pess_nt[nome] = nota  
 
 nota_esp = float(input())
 
-# Coleta os nomes das pessoas que possuem a nota específica
 nomes_com_nota_esp = [nome for nome, nota in pess_nt.items() if nota == nota_esp]
 nomes_com_nota_esp.sort()
 if len(nomes_com_nota_esp) == 0:
     print(f'Você foi o único aluno com essa nota.')
-# Imprimir os nomes separados por '/'
+# imrpime os nomes separados por '/'
 print('/'.join(nomes_com_nota_esp))
 
     
